b669b37daeb4ee9632f22ef053b7b3c47f3314bb
mohamm-alsaid <mohamm.alsaid@gmail.com>
2022-03-27 06:38:19 -0700

    add logic for GET MVoTs

--- CDTA.py
+++ CDTA.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, request
-import sqlite3
-from dict2xml import dict2xml
+from flask import Flask, redirect, request, Response
+import sqlite3, xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 app = Flask(__name__)
 
@@ -9,27 +9,45 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def dict_to_xml(tag, d):
+    elem = ET.Element(tag)
+    for key, val in d.items():
+        # create an Element
+        # class object
+        child = ET.Element(key)
+        child.text = str(val)
+        elem.append(child)
+    return elem
+
 def mvots_to_xml(mvots:list):
-    for mvot in mvots:
-        print(dict2xml(dict(mvot)))
+    root = ET.Element("MVoTs")
+    root = ET.SubElement(root,"MVoTs")
+    root.tag = 'MVoTs'
+    for mvot in mvots[:10]:
+        root.append(dict_to_xml('MVoT',dict(mvot)))
+
+    return root
 
 @app.route('/MVoT',methods=['POST','GET'])
 def index():
-    if request.method == 'POST':
+    conn = get_db_connection()
+    if request.method == 'GET':
+        mvots = conn.execute('SELECT * FROM MVoTs').fetchall()
+        
+        # call create xml for all rows in db
+        xml = ET.tostring(mvots_to_xml(mvots),encoding='unicode',method='xml')
+        
+        print(minidom.parseString(xml).toprettyxml(indent="   "))
+
+        response = Response(minidom.parseString(xml).toprettyxml(indent="   "),mimetype='text/xml')
+    else:
+        # when request.method == 'POST' 
         # call extract mvot_xml
         print('post request')
-        return 'post'
-        pass
-    elif request.method == 'GET':
-        # call create xml for all rows in db
-        conn = get_db_connection()
-        conn = get_db_connection()
-        mvots = conn.execute('SELECT * FROM MVoTs').fetchall()
-        conn.close()
-        
-        mvots_to_xml(mvots)
-        return 'mvots'
-    return 'error!'
+        response = ''
+    
+    conn.close()
+    return response
 
 
 if __name__ == '__main__':
